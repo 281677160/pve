669f944e8e2a664b63ca80d5584b1d7f0f690964
281677160 <66651997+281677160@users.noreply.github.com>
2021-12-28 20:28:43 +0800

    Update pvehy.sh

--- pvehy.sh
+++ pvehy.sh
@@ -17,11 +17,6 @@ TIME() {
 	 }
       }
 }
-echo
-echo
-TIME g "正在更换源...!"
-echo
-echo
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 echo '
 # 阿里云源（升级不了版本）
@@ -39,7 +34,14 @@ deb http://deb.debian.org/debian bullseye-updates main contrib
 deb http://security.debian.org bullseye-security main contrib
 ' > /etc/apt/sources.list
 sed -i '1d' /etc/apt/sources.list
-
+echo
+TIME g "升级PVE"
+echo
+apt update -y && apt install -y net-tools curl wget
+apt dist-upgrade -y
+echo
+TIME g "更换源"
+echo
 cp -fr /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
 rm -rf /etc/apt/sources.list.d/pve-enterprise.list
 echo "deb http://mirrors.ustc.edu.cn/proxmox/debian/pve bullseye pve-no-subscription" >/etc/apt/sources.list.d/pve-install-repo.list
@@ -60,26 +62,16 @@ if [[ $? -ne 0 ]];then
     	fi
 fi
 echo
-echo
 TIME g "去掉无效订阅"
-echo
 echo
 cp /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js.bak
 sed -i 's#if (res === null || res === undefined || !res || res#if (false) {#g' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 sed -i '/data.status.toLowerCase/d' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 echo
 echo
-TIME g "更新源、安装常用软件和升级"
-echo
-echo
-apt-get update && apt-get install -y net-tools curl git
-apt dist-upgrade -y
-sleep 3
 sed -i 's#http://download.proxmox.com#https://mirrors.ustc.edu.cn/proxmox#g' /usr/share/perl5/PVE/APLInfo.pm
 echo
-echo
 TIME g "更换DNS为223.5.5.5和114.114.114.114"
-echo
 echo
 cp /etc/resolv.conf /etc/resolv.conf.bak
 echo '
@@ -90,8 +82,6 @@ nameserver 114.114.114.114
 sed -i '1d' /etc/resolv.conf
 rm -fr /root/pvehy.sh
 echo
-echo
 TIME g "重启PVE，需要几分钟时间，请耐心等候..."
-echo
 echo
 reboot
