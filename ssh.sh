45140c804a11265500c2a15760868ec21a34182b
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-07 09:35:37 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -7,18 +7,14 @@ fi
 
 function system_check() {
   if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
-    yum install -y sudo
     system_centos
   elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
     apt -y update
-    apt-get install -y sudo
     system_ubuntu
   elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
     apt -y update
-    apt install -y sudo
     system_debian
   elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
-    apk add sudo
     system_alpine
   else
     echo -e "\033[41;33m 不支持您的系统  \033[0m"
@@ -32,10 +28,10 @@ function system_centos() {
     yum install -y openssh-server
     systemctl enable sshd.service
     ssh_PermitRootLogin
-    sudo service sshd restart
+    service sshd restart
   else
     ssh_PermitRootLogin
-    sudo service sshd restart
+    service sshd restart
   fi
   echo -e "\033[32m 开启root账户SSH完成 \033[0m"
   exit 0
@@ -46,10 +42,10 @@ function system_ubuntu() {
     echo -e "\033[33m 安装SSH \033[0m"
     apt-get install -y openssh-server
     ssh_PermitRootLogin
-    sudo service ssh restart
+    service ssh restart
   else
     ssh_PermitRootLogin
-    sudo service ssh restart
+    service ssh restart
   fi
   echo -e "\033[32m 开启root账户SSH完成 \033[0m"
   exit 0
@@ -60,10 +56,10 @@ function system_debian() {
     echo -e "\033[33m 安装SSH \033[0m"
     apt install -y openssh-server
     ssh_PermitRootLogin
-    sudo service ssh restart
+    service ssh restart
   else
     ssh_PermitRootLogin
-    sudo service ssh restart
+    service ssh restart
   fi
   echo -e "\033[32m 开启root账户SSH完成 \033[0m"
   exit 0
@@ -76,10 +72,10 @@ function system_alpine() {
     apk add openssh-client
     rc-update add sshd
     ssh_PermitRootLogin
-    sudo service sshd restart
+    service sshd restart
   else
     ssh_PermitRootLogin
-    sudo service sshd restart
+    service sshd restart
   fi
   echo -e "\033[32m 开启root账户SSH完成 \033[0m"
   exit 0
@@ -87,12 +83,12 @@ function system_alpine() {
 
 function ssh_PermitRootLogin() {
   if [[ `grep -c "ClientAliveInterval 30" /etc/ssh/sshd_config` == '0' ]]; then
-    sudo sed -i '/ClientAliveInterval/d' /etc/ssh/sshd_config
-    sudo sed -i '/ClientAliveCountMax/d' /etc/ssh/sshd_config
-    sudo sh -c 'echo ClientAliveInterval 30 >> /etc/ssh/sshd_config'
-    sudo sh -c 'echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config'
+    sed -i '/ClientAliveInterval/d' /etc/ssh/sshd_config
+    sed -i '/ClientAliveCountMax/d' /etc/ssh/sshd_config
+    sh -c 'echo ClientAliveInterval 30 >> /etc/ssh/sshd_config'
+    sh -c 'echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config'
   fi
-  sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-  sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+  sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+  sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 }
 system_check "$@"
